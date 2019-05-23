73345ec5ff21230851ecec56a3b713408ff1d031
JeminSuvagiya <200372988@student.georgianc.on.ca>
2019-05-23 16:11:17 -0400

    simple node server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`Hey 'dare world!`);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`I like swimming`);
+});
+
+app.listen(4000, () => console.log('Listening on 4000'));
